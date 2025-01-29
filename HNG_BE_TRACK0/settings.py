c76869aaf01e8e31487500d7c548d5bccdc538fb
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-01-29 11:39:30 +0100

    Added the use of django environments

--- HNG_BE_TRACK0/settings.py
+++ HNG_BE_TRACK0/settings.py
@@ -11,19 +11,25 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+env = environ.Env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g$!tg(ae+u1+%_phayd7t6+(&kavch3b*!xe(rxn42z&38u9-%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 

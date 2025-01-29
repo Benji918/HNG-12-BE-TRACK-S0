27d262d41e9e5fa750df7a69a34f92ffe02edfc9
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-01-29 13:19:09 +0100

    file changes(configured whitenoise properly)

--- HNG_BE_TRACK0/settings.py
+++ HNG_BE_TRACK0/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'HNG_BE_TRACK0.urls'

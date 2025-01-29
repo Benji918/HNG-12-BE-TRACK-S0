3bcf8fbfd96ad8f70999c79fe9a59b353f421b47
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-01-29 12:25:25 +0100

    file changes

--- HNG_BE_TRACK0/settings.py
+++ HNG_BE_TRACK0/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [env('DEPLOYMENT_URL'), '127.0.0.1', 'hng-12-be-track-s0.onrender.com']
+ALLOWED_HOSTS = [env('DEPLOYMENT_URL'), '127.0.0.1']
 
 
 # Application definition
